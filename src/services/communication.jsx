56eb903db4d5915067b8f7ea68a1bbd6c2e7d2ec
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-16 17:01:45 +0200

    adding createAnecdote and voteAnecdote function to do post and put operation

--- src/services/communication.jsx
+++ src/services/communication.jsx
@@ -1,6 +1,15 @@
 import axios from 'axios'
+const baseUrl='http://localhost:3001/anecdotes'
 
-export const getAll=()=>axios.get('http://localhost:3001/anecdotes')
-  .then(response=>response.data)
+export const getAll=()=>axios.get(baseUrl)
+  .then(res=>res.data)
+
+export const createAnecdote=(newObject)=>axios.post(baseUrl,newObject)
+  .then(res=>res.data)
+
+export const voteAnecdote=(votedAnecdote)=>axios.put(`${baseUrl}/${votedAnecdote.id}`,votedAnecdote)
+  .then(res=>res.data)
+
+
 
 
