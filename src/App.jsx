be0fdc2b8306f36ea53642ea7d6ad8d465f768f9
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-16 16:44:46 +0200

    importing useMutation to create anecdote as async operation and using useContext hook api

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,68 @@
+import  Anecdote  from './components/Anecdote'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
-import { getAll } from './services/communication'
+import { useQuery,useMutation,useQueryClient } from '@tanstack/react-query'
+import { getAll,voteAnecdote } from './services/communication'
+import { useContext } from 'react'
+import NotificationContext from './components/NotificationContext'
+
 
 const App = () => {
+  const queryClient=useQueryClient()
+  const [,notificationDispatch]=useContext(NotificationContext)
+
+  const result=useQuery({
+    queryKey:['anecdotes'],
+    queryFn:getAll,
+    retry:false,
+    refetchOnWindowFocus:false
+  })
+
+  const voteAnecdoteMutation=useMutation({
+    mutationFn:voteAnecdote,
+    onSuccess:(changedAnecdote)=>{
+    //queryClient.invalidateQueries('anecdotes')//automatic updating but increases server work load
+      const anecdotes=queryClient.getQueryData(['anecdotes']) //manual code but lessen server workload(optimization)
+      queryClient.setQueryData(['anecdotes'],
+        anecdotes.map(anecdote=>anecdote.id!==changedAnecdote.id
+          ?anecdote
+          :changedAnecdote
+        )
+      )
+    }
+  })
+  /*const {isPending,isError,data,error}=useQuery({
+    queryKey:['anecdotes'],
+    queryFn:getAll,
+    retry:false //prevent  useQuery from doing multiple request
+  })*/
+  //console.log(JSON.parse(JSON.stringify(result)))
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    const votedAnecdote={...anecdote,votes:anecdote.votes+1}
+    voteAnecdoteMutation.mutate(votedAnecdote)
+    notificationDispatch({type:'SETVOTE',payload: anecdote.content})
+    setTimeout(()=>{
+      notificationDispatch({type:'REMOVE'})
+    },5000)
   }
   
-  /*const result=useQuery({
-    queryKey:['anecdotes'],
-    queryFn:getAll
-  })*/
-  const {isPending,isError,data,error}=useQuery({
-    queryKey:['anecdotes'],
-    queryFn:getAll
-  })
-  
-  if(isPending)
+  if(result.isPending)
   return<p>Loading...</p>
   
-  if(isError)
+  if(result.isError)
   return <p>Error : Anecdote service not available due to problems in server</p>
   return (
     <div>
       <h3>Anecdote app</h3>
-    
       <Notification />
-      <AnecdoteForm />
-    
-      {data.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteForm />  
+      {result.data.map(anecdote => 
+        <Anecdote 
+          key={anecdote.id} 
+          anecdote={anecdote} 
+          handleVote={handleVote}/>
+      )} 
     </div>
   )
 }
