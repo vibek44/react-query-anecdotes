617c02b9deb16a3a28b19cd8aa06d63a67bb044f
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-16 16:57:54 +0200

    importing useContext and NotificationContext to access global context defined elsewhere

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,16 +1,21 @@
+import { useContext } from 'react'
+import NotificationContext from './NotificationContext'
 const Notification = () => {
+  
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
     marginBottom: 5
   }
+
+  const [ notification, ]=useContext(NotificationContext)
   
-  if (true) return null
+  if (!notification) return null
 
   return (
     <div style={style}>
-      
+      {notification}
     </div>
   )
 }
